ba816a152de52338d45a068805ee81db3916e8c4
btwise <38809560+btwise@users.noreply.github.com>
2023-02-09 17:22:58 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,20 @@
 /* main.cpp --
-
    This file is part of the UPX executable compressor.
-
    Copyright (C) 1996-2023 Markus Franz Xaver Johannes Oberhumer
    Copyright (C) 1996-2023 Laszlo Molnar
    All Rights Reserved.
-
    UPX and the UCL library are free software; you can redistribute them
    and/or modify them under the terms of the GNU General Public License as
    published by the Free Software Foundation; either version 2 of
    the License, or (at your option) any later version.
-
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-
    You should have received a copy of the GNU General Public License
    along with this program; see the file COPYING.
    If not, write to the Free Software Foundation, Inc.,
    59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-
    Markus F.X.J. Oberhumer              Laszlo Molnar
    <markus@oberhumer.com>               <ezerotven+github@gmail.com>
  */
@@ -1288,7 +1282,7 @@ int upx_main(int argc, char *argv[]) {
             FILE *f = stdout;
             int fg = con_fg(f, FG_RED);
             con_fprintf(
-               f, "\nWARNING: this is an unstable beta version - use for testing only! Really.\n");
+                f, "\nWARNING: this is an unstable beta version - use for testing only! Really.\n");
             fg = con_fg(f, fg);
             UNUSED(fg);
         }
